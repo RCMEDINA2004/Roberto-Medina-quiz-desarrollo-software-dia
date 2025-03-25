77666d4b94f9052a9f7f9f1332ae0440fe5c6e1a
RC_MEDINA <medina.roberto.3800@eam.edu.co>
2025-03-25 07:42:10 -0500

    Roberto Medina: inicio proyecto con Spring Boot

---
+++ src/main/java/com/example/QuizDesarrolloSoftwareRC/QuizDesarrolloSoftwareRcApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuizDesarrolloSoftwareRC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizDesarrolloSoftwareRcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizDesarrolloSoftwareRcApplication.class, args);
+	}
+
+}
